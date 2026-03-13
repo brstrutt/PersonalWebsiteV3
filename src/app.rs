b7cd03c07994e36c74d877e557a03b8855af4b22
brstrutt <ben.strutt@hotmail.co.uk>
2026-03-13 15:26:10 +0000

    add custom progress bar with props, optional props, and documentation

--- src/app.rs
+++ src/app.rs
@@ -8,15 +8,25 @@ pub fn App() -> impl IntoView {
         <button
             on:click=move |_| *set_count.write() += 1
             class=("bold", move || count.get() % 2 == 0)
-            style=("font-size", move || format!("{}px", count.get()))
+            style=("font-size", move || format!("{}px", count.get() + 10))
         >
             "Click me: "
             {count}
         </button>
-        <progress
-            max="50"
-            value=count
-            style=("display", "block")
-        />
+        <ProgressBar progress=move || count.get() />
+        <ProgressBar max=10 progress=count />
     }
+}
+
+/// A custom progress bar with custom default styling/values
+#[component]
+fn ProgressBar(
+    /// The current value in the progress bar
+    #[prop(into)]
+    progress: Signal<i32>,
+    /// The value at which the progress bar reaches 100%
+    #[prop(default = 50)]
+    max: u16,
+) -> impl IntoView {
+    view! { <progress max=max value=progress style=("display", "block") /> }
 }
