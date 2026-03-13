1fe809a78f56dd592ca5ea4967c39d7b0f3fba39
brstrutt <ben.strutt@hotmail.co.uk>
2026-03-13 18:07:52 +0000

    replace text links with icon links in the footer (and make it look better)

--- src/app/components/footer.rs
+++ src/app/components/footer.rs
@@ -4,8 +4,12 @@ use leptos::prelude::*;
 pub fn Footer() -> impl IntoView {
     view! {
         <footer>
-            <a href="https://github.com/brstrutt" target="_blank" rel="noopener noreferrer">"Github"</a>
-            <a href="https://www.linkedin.com/in/ben-strutt-09b9a280/" target="_blank" rel="noopener noreferrer">"LinkedIn"</a>
+            <a href="https://github.com/brstrutt" target="_blank" rel="noopener noreferrer">
+                <img src="assets/images/GitHub-Mark-Light-64px.png" title="Github" alt="Github Logo" width="32px" height="32px"/>
+            </a>
+            <a href="https://www.linkedin.com/in/ben-strutt-09b9a280/" target="_blank" rel="noopener noreferrer">
+                <img src="assets/images/LI-In-Bug-Modified.png" title="LinkedIn" alt="LinkedIn Logo(Slightly Modified)" width="32px" height="32px"/>
+            </a>
         </footer>
     }
 }
