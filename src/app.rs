2a6238db4e4d9e62fc9e2782bd52148c85d8ae7c
brstrutt <ben.strutt@hotmail.co.uk>
2026-03-13 15:05:15 +0000

    add custom styles and styles that change dynamically

--- src/app.rs
+++ src/app.rs
@@ -5,7 +5,15 @@ pub fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
 
     view! {
-        <button on:click=move |_| *set_count.write() += 1>"Click me: " {count}</button>
+        <button
+            on:click=move |_| *set_count.write() += 1
+            class=("green", move || count.get() % 2 == 0)
+            class=("red", move || count.get() % 2 == 1)
+            style=("font-size", move || format!("{}px", count.get()))
+        >
+            "Click me: "
+            {count}
+        </button>
         <p>"Double count: " {move || count.get() * 2}</p>
     }
 }
