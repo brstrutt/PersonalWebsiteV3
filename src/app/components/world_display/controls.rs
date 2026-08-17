d5dbe35dfe245f84360ea5419a6754a00eebf166
brstrutt <ben.strutt@hotmail.co.uk>
2026-08-17 20:06:29 +0000

    feat(world_display): add touchscreen camera movement

--- src/app/components/world_display/controls.rs
+++ src/app/components/world_display/controls.rs
@@ -3,10 +3,7 @@ use leptos::leptos_dom;
 use wasmfenbein3d::core::state::GameState;
 use leptos::web_sys::{HtmlCanvasElement, EventTarget};
 use leptos::ev::{
-    self,
-    Event,
-    KeyboardEvent,
-    MouseEvent
+    self, Event, KeyboardEvent, MouseEvent, TouchEvent
 };
 use leptos::prelude::*;
 use crate::app::utils::add_event_listener_with_callback;
@@ -67,5 +64,55 @@ pub fn setup_controls(canvas_element: HtmlCanvasElement, state: Rc<RefCell<GameS
                 &_ => return,
             }
         }
+    });
+
+    setup_camera_touch_control(canvas_element, state);
+}
+
+fn setup_camera_touch_control(canvas_element: HtmlCanvasElement, state: Rc<RefCell<GameState>>) {
+    let cloned_state = state.clone();
+    add_event_listener_with_callback(EventTarget::from(canvas_element.clone()), "touchstart", move |e: TouchEvent| {
+        e.prevent_default();
+        let mut state = cloned_state.borrow_mut();
+
+        let touch_points = e.target_touches();
+        if touch_points.length() > 0 {
+            let touch_x_position = touch_points
+                .item(0)
+                .expect("Failed to get first touch point on the canvas")
+                .screen_x();
+            state.input.last_canvas_touch_point_x = Some(touch_x_position);
+        }
+    });
+
+    let cloned_state = state.clone();
+    add_event_listener_with_callback(EventTarget::from(canvas_element.clone()), "touchmove", move |e: TouchEvent| {
+        e.prevent_default();
+        let mut state = cloned_state.borrow_mut();
+        const ACCELERATION: i32 = 4;
+
+        let touch_points = e.target_touches();
+        if touch_points.length() > 0 {
+            let touch_x_position = touch_points
+                .item(0)
+                .expect("Failed to get first touch point on the canvas")
+                .screen_x();
+
+            if state.input.last_canvas_touch_point_x.is_some() {
+                state.input.camera_rotation = (state.input.last_canvas_touch_point_x.unwrap()
+                    - touch_x_position)
+                    * ACCELERATION;
+            }
+
+            state.input.last_canvas_touch_point_x = Some(touch_x_position);
+            state.input.touch_has_moved_camera = true;
+        }
+    });
+
+    let cloned_state = state.clone();
+    add_event_listener_with_callback(EventTarget::from(canvas_element), "touchend", move |e: TouchEvent| {
+        e.prevent_default();
+        let mut state = cloned_state.borrow_mut();
+        state.input.last_canvas_touch_point_x = None;
     });
 }
